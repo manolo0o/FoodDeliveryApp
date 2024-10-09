b403c59ad86baa66b59510fcf61c95be96e4b969
manolo0o <147361804+manolo0o@users.noreply.github.com>
2024-10-09 15:34:12 -0500

    feat: :construction: Working on the structure of the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// route importations
+import Start from './routes/Start.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Start />,
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
