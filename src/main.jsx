72b9f422f411335fd2ddce8f1eb430bd7c7c259e
manolo0o <147361804+manolo0o@users.noreply.github.com>
2024-10-09 16:40:24 -0500

    feat: :white_check_mark: UI design first View

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+
 // route importations
 import Start from './routes/Start.jsx';
 
--- src/routes/Start.jsx
+++ src/routes/Start.jsx
@@ -1,11 +1,31 @@
+// DEPENDENCIES IMPORTATIONS
 import React from "react";
 import { Link } from "react-router-dom";
 
+// STYLES IMPORTATION
+import "../css/Start.css";
+
+//IMAGE IMPORTATION
+import backgroundImage1 from "../assets/img/269.png";
+import backgroundImage2 from "../assets/img/startIcon.png";
+import continueButton from "../assets/img/continue.png";
+import options from "../assets/img/options.png";
+
+// SCRIPT
 function Start() {
   return (
     <>
         <div className="StartContainer">
-            
+            <img src={backgroundImage1} className="backgroundImage1"/> 
+            <img src={backgroundImage2} className="backgroundImage2"/> 
+                <div className="buttonContainer">
+                    <button>
+                        <img src={options} alt="options" />
+                    </button> 
+                    <button>
+                        <img src={continueButton} alt="Continue" />
+                    </button>
+                </div>
         </div>
     </>
   );
