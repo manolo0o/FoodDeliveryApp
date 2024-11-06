c5ca5aec4bd6ec1bd54ab05bc5c14591a7957252
manolo0o <147361804+manolo0o@users.noreply.github.com>
2024-11-05 22:34:11 -0500

    feat: :adhesive_bandage: mongooseSchema

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,52 +1,39 @@
 const mongoose = require('mongoose');
 
-const productSchema =  new mongoose.Schema(
-    {
+const productSchema =  new mongoose.Schema({
         product__image : {
             type : 'string',
             required : true,
-        }
-    },
-    {
+        },
         product__Name : {
             type : String,
             required : true,
-        }
-    },
-    {
+        },
         product__Description : {
             type : String,
             required : true,
-        }
-    },
-    {
+        },
         product__ShortDescription : {
             type : String,
             required : true,
-        }
-    },
-    {   
+        },
         product__Price : {
             type : Number,
             required : true,
-        }
-    },
-    {
+        },
         product__Category : {
             type : String,
             required : true,
-        }
-    },
-    {
+        },
         product__Food__Time : { 
             type : String,
             required : true,
-        }
-    },
-    {
+        },
         product__calories : {
             type : String,
             required : true
         }
-    }
-)
+    });
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
