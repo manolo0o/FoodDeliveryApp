5e759ec31c844c0071f0fd6b6e6e6dc6be175775
manolo0o <147361804+manolo0o@users.noreply.github.com>
2024-11-22 16:32:01 -0500

    feat: :white_check_mark: Delivery Card component completly done.

--- src/components/deliveryCard.jsx
+++ src/components/deliveryCard.jsx
@@ -12,14 +12,16 @@ function DeliveryCard() {
 
     return (
         <div className="deliveryCard__Container">
-            <div className="lContent">
-                <h2>Delivery Amount</h2>
-                <p>total amount</p>
-                <h2>USD 3.00</h2>
+          <div className="lContent">
+            <h2>Delivery Amount</h2>
+            <p>Total Amount</p>
+            <h2 className="Price">USD 3.00</h2>
+          </div>
+          <div className="rContent">
+            <div className="containerPrice">
+              <p>$ 3.00</p>
             </div>
-            <div className="rContent">
-                <p>3.00</p>
-            </div>            
+          </div>            
         </div>
     )
 }
